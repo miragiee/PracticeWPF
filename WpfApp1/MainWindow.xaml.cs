ea05deddff377bbce5a37cbece14e55917cbfa35
Спиридонов Кирилл Сергеевич <10230922@college.local>
2025-11-24 15:44:28 +0300

    Добавил админ панель, вход в админ панель. А так же GotFocused и LostFocus для TextBox на странице логина MainWindow

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,16 +16,48 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string login;
+        public string password;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        // Временный алгоритм авторизации
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            GoodsMain goodsMain = new GoodsMain();
-            goodsMain.Show();
-            this.Close();
+            login = LoginBox.Text;
+            password = PasswordBox.Text;
+
+            if (login != "" && password != "")
+            {
+                if (login == "admin" && password == "123456")
+                {
+                    AdminPanel admPanel = new AdminPanel();
+                    admPanel.Show();
+                    this.Close();
+                }
+                else
+                {
+                    GoodsMain goodsMain = new GoodsMain();
+                    goodsMain.Show();
+                    this.Close();
+                    
+                }
+            }
+            //else
+            //{
+            //    if (LoginBox.Text == "Почта | Логин")
+            //    {
+            //        LoginBox.Text = "Введите логин*";
+            //        LoginBox.Foreground = Brushes.Red;
+            //    }
+            //    if (PasswordBox.Text == "Пароль")
+            //    {
+            //        PasswordBox.Text = "Введите пароль*";
+            //        PasswordBox.Foreground = Brushes.Red;
+            //    }
+            //}
         }
 
         private void MoveToRegister(object sender, RoutedEventArgs e)
@@ -33,6 +65,38 @@ namespace WpfApp1
             Register register = new Register();
             register.Show();
             this.Close();
+        }
+
+        private void Login_Focused(object sender, RoutedEventArgs e)
+        {
+            if(LoginBox.Text == "Почта | Логин")
+            {
+                LoginBox.Text = string.Empty;
+            }
+        }
+
+        private void LoginNotFocused(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginBox.Text))
+            {
+                LoginBox.Text = "Почта | Логин";
+            }
+        }
+
+        private void Password_Focused(object sender, RoutedEventArgs e)
+        {
+            if(PasswordBox.Text == "Пароль")
+            {
+                PasswordBox.Text = string.Empty;
+            }
+        }
+
+        private void PasswordNotFocused(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(PasswordBox.Text))
+            {
+                PasswordBox.Text = "Пароль";
+            }
         }
     }
 }
