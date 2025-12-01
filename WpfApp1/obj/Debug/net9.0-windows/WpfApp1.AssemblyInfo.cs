e0631259355661b38335739de16876d9338348de
miragiee <miragiee564@gmail.com>
2025-12-01 15:52:43 +0300

    changed readme
    Рот болит и попе больно -- на WPF писать прикольно

--- WpfApp1/obj/Debug/net9.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net9.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3edab1f3e6ac821864c138994cff1d8414e2072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9aa7988258b8605ba73268034fe5071a6b44ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
